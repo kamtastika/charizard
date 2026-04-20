6e8b44110d4dbb0dc7d7752aaf7fa0659ff5b622
kamtastika <cafunekami@gmail.com>
2026-04-20 09:40:07 -0400

    concatenaciones

--- mas ejercisios.py
+++ mas ejercisios.py
@@ -6,7 +6,15 @@ pkm1=6
 entrenador2="sabrina"
 pkm2=7
 
-if pkm1<=6
+if pkm1<=6:
     print("Puede acceder a la liga pokemon")
 else:
     print("tiene mas pokemon de los necesarios")
+
+
+print(len(entrenador))
+
+
+print("el entrenador", entrenador, "tiene", pkm1, "pokemons")
+print("el entrenador", entrenador, "tiene", pkm2, "pokemons")
+print(f"el entrenador {entrenador}, tiene {pkm2} pokemons)

3131332f28f8a27828a303f18541b4a6c43e19c5
kamtastika <cafunekami@gmail.com>
2026-04-20 10:10:36 -0400

    concatenaciones2

--- mas ejercisios.py
+++ mas ejercisios.py
@@ -1,20 +1,26 @@
 
-entrenador= "kami"
+# entrenador= "kami"
 
-pkm1=6
+# pkm1=6
 
-entrenador2="sabrina"
-pkm2=7
+# entrenador2="sabrina"
+# pkm2=7
 
-if pkm1<=6:
-    print("Puede acceder a la liga pokemon")
-else:
-    print("tiene mas pokemon de los necesarios")
-
-
-print(len(entrenador))
+# if pkm1<=6:
+#     print("Puede acceder a la liga pokemon")
+# else:
+#     print("tiene mas pokemon de los necesarios")
 
 
-print("el entrenador", entrenador, "tiene", pkm1, "pokemons")
-print("el entrenador", entrenador, "tiene", pkm2, "pokemons")
-print(f"el entrenador {entrenador}, tiene {pkm2} pokemons)
+# print(len(entrenador))
+
+
+# print("el entrenador", entrenador, "tiene", pkm1, "pokemons")
+# print("el entrenador", entrenador, "tiene", pkm2, "pokemons")
+# print(f"el entrenador {entrenador}, tiene {pkm2} pokemons)
+
+# print(f"Hola {entrenador}"*3)
+name="ana"
+#     012
+print(name[2])
+# el ultimo siempre es -1
